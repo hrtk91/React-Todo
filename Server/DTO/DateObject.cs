b0800a1f617581708e516ab1502e36d09c8ef650
hrtk91 <ht91218@gmail.com>
2022-05-14 17:33:48 +0900

    Develop (#4)
    * 要求仕様のAPIを実装
    
    * bootstrap追加
    
    * localhostでのcors有効化
    
    * 画面側一部作成
    
    * フロント側を大方作成
    
    * cos設定の修正
    
    * 不要属性削除
    
    * 日時やり取り専用のDTO作成
    
    * 引数にDTO使用するよう変更、IDでdescソートした順序で返すよう変更
    
    * デプロイ時のcors設定を追加
    
    * リリース用の設定追加
    
    * コンポーネントのフォルダに移動
    
    * 表示の調整
    
    * 表示を調整
    
    * 不要ファイルの削除
    
    * コメント付加
    
    * 期限切れの判定ロジック修正
    
    * DateObjectに検証処理追加

--- Server/DTO/DateObject.cs
+++ Server/DTO/DateObject.cs
@@ -1,20 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Server.DTO;
 
+/// <summary>
+/// 日時オブジェクトのDTOクラス
+/// </summary>
 public class DateObject
 {
+    /// <summary>年</summary>
     public int Year { get; set; }
+    /// <summary>月</summary>
     public int Month { get; set; }
+    /// <summary>日</summary>
     public int Day { get; set; }
+    /// <summary>時</summary>
     public int Hour { get; set; }
+    /// <summary>分</summary>
     public int Minute { get; set; }
+    /// <summary>秒</summary>
     public int Second { get; set; }
+    /// <summary>ミリ秒</summary>
     public int MilliSecond { get; set; }
 
+    /// <summary>
+    /// すべてのプロパティを0初期化してインスタンスを作成します
+    /// </summary>
+    /// <remarks>
+    /// ApiControllerでインスタンス作成する際、プロパティなしのコンストラクタが必要なため用意しています。<br/>
+    /// コードからは使用しないでください。
+    /// </remarks>
     public DateObject()
     {
         this.Year = this.Month = this.Day = this.Hour = this.Minute = this.Second = this.MilliSecond = 0;
     }
 
+    /// <summary>
+    /// 日時を指定してインスタンスを初期化します
+    /// </summary>
+    /// <param name="year">年</param>
+    /// <param name="month">月</param>
+    /// <param name="day">日</param>
+    /// <param name="hour">時</param>
+    /// <param name="minute">分</param>
+    /// <param name="second">秒</param>
+    /// <param name="MilliSecond">ミリ秒</param>
     public DateObject(int year, int month, int day, int hour, int minute, int second, int MilliSecond)
     {
         this.Year = year;
@@ -26,12 +55,72 @@ public class DateObject
         this.MilliSecond = MilliSecond;
     }
 
+    /// <summary>
+    /// DateTimeオブジェクトの値からインスタンスを初期化します
+    /// </summary>
+    /// <param name="date">日時</param>
     public DateObject(DateTime date)
         : this(date.Year, date.Month, date.Day, date.Hour, date.Minute, date.Second, date.Millisecond)
     {
 
     }
 
+    /// <summary>
+    /// DateTimeオブジェクトに変換します
+    /// </summary>
+    /// <returns>DateTimeオブジェクト</returns>
     public DateTime ToDateTime()
         => new DateTime(this.Year, this.Month, this.Day, this.Hour, this.Minute, this.Second, this.MilliSecond);
+
+    /// <summary>
+    /// DateObjectプロパティの検証属性
+    /// </summary>
+    public class ValidateAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// nullable宣言されたプロパティか
+        /// </summary>
+        private bool IsNullable { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="isNullable">属性の適用先がnullableか</param>
+        public ValidateAttribute(bool isNullable = false)
+        {
+            this.IsNullable = isNullable;
+        }
+
+        /// <summary>
+        /// DateObjectがDateTimeに変換可能か検証し、変換可能な場合trueを返します。<br/>
+        /// ただし、isNullableがtrueでプロパティがnullの場合にもtrueを返します。
+        /// </summary>
+        /// <param name="value">属性の付与先プロパティ</param>
+        /// <returns>検証結果</returns>
+        public override bool IsValid(object? value)
+        {
+            // IsNullableがtrueの場合、null値は検証OK
+            if (value is null)
+            {
+                return IsNullable;
+            }
+
+            // DateObjectプロパティに付与されていなければ検証NG
+            if (value is not DateObject dObj)
+            {
+                return false;
+            }
+
+            // DateTime型に変換できれば検証OK
+            try
+            {
+                dObj.ToDateTime();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
 }
--- Server/DTO/Todo.cs
+++ Server/DTO/Todo.cs
@@ -2,20 +2,36 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Server.DTO;
 
+/// <summary>
+/// TodoのDTOクラス
+/// </summary>
 public class Todo
 {
+    /// <summary>ID</summary>
     public int Id { get; set; }
 
+    /// <summary>内容</summary>
     [Required]
     [MaxLength(100)]
     public string Content { get; set; } = string.Empty;
 
+    /// <summary>予定日</summary>
+    [DateObject.Validate]
     public DateObject DueDate { get; set; } = new DateObject(DateTime.Today);
 
+    /// <summary>作成日</summary>
+    [DateObject.Validate]
     public DateObject Created { get; set; } = new DateObject(DateTime.Now);
 
+    /// <summary>完了日</summary>
+    [DateObject.Validate(true)]
     public DateObject? DoneAt { get; set; } = null;
 
+    /// <summary>
+    /// TodoモデルからTodoのDTOに変換します
+    /// </summary>
+    /// <param name="todo">Todoモデル</param>
+    /// <returns>TodoのDTO</returns>
     public static DTO.Todo From(Models.Todo todo)
     {
         return new DTO.Todo
